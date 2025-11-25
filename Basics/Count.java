610c508e82216256f75088253fb5e4f3a94bf4e0
Aayush240305 <pawarpravin99163@gmail.com>
2025-11-25 11:36:32 +0530

    Count

---
+++ Basics/Count.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Count{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter a number : ");
+    int num = sc.nextInt();
+    int count = 0;
+    
+    if(num < 0){
+      num = - num;
+    }
+    
+    if(num == 0){
+      count++;
+    }else{
+      while(num > 0){
+        num = num / 10;
+        count++;
+      }
+    }
+    
+    System.out.println("count of number is :" + count);
+}
+}
