2c81fa7852d2829d64de69920c880263a509a525
Aayush240305 <pawarpravin99163@gmail.com>
2025-11-25 13:21:46 +0530

    Second Largest

---
+++ Arrays/SecondLargest.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class SecondLargest{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a size of array : ");
+    int size = sc.nextInt();
+    int[] arr = new int[size];
+    System.out.println("Enter a elements of array : ");
+    for(int i = 0; i<size; i++){
+      arr[i] = sc.nextInt();
+    }
+    
+    for(int i = 0; i < size - 1; i++){
+      for(int j = 0; j < size - i - 1; j++){
+        if(arr[j] > arr[j+1]){
+        int temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+      }
+    }
+    
+    System.out.println("Second largest element is : " + arr[size - 2]);
+  }
+}
